1c8fe97d17fefc680c04184045cc01078f934443
Shubhamsharma09 <shubhamsh509@gmail.com>
2020-07-21 22:49:14 +0530

    Validation of Model Object

--- ppmtool/src/main/java/shushar/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/shushar/ppmtool/domain/Project.java
@@ -1,12 +1,16 @@
 package shushar.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Project {
@@ -15,13 +19,26 @@ public class Project {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+
+  @NotBlank(message = "Project Name is required")
   private String projectName;
+
+  @NotBlank(message = "Project Identifier is required")
+  @Size(min=4, max=5, message="Please use 4 to 5 Characters")
+  @Column(updatable = false, unique = true)
   private String projectIdentifier;
+
+  @NotBlank(message = "Project Description required")
   private String description;
+
+  @JsonFormat(pattern ="yyyy-mm-dd")
   private Date start_date;
+  @JsonFormat(pattern ="yyyy-mm-dd")
   private Date end_date;
 
+  @JsonFormat(pattern ="yyyy-mm-dd")
   private Date created_At;
+  @JsonFormat(pattern ="yyyy-mm-dd")
   private Date updated_At;
 
   public Project() {
