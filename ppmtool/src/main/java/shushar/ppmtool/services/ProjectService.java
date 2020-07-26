ff205f6e60cd1939e309e248db20a1c340f2f87f
Shubhamsharma09 <shubhamsh509@gmail.com>
2020-07-26 21:20:56 +0530

    Exception handler

--- ppmtool/src/main/java/shushar/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/shushar/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ package shushar.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import shushar.ppmtool.domain.Project;
+import shushar.ppmtool.exceptions.ProjectIdException;
 import shushar.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -16,8 +17,15 @@ public class ProjectService {
 // Passing simple project obj and going to be persisted on the db.
     //Logic
 
-
+try{
+  project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
     return projectRepository.save(project);
-    }
+}catch(Exception e)
+{
+  throw new ProjectIdException("Project ID "+project.getProjectIdentifier().toUpperCase()+"already exists");
+}
+
+
+}
 
 }
